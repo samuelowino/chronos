7890775cf398547ff069e196c426ac65a46abe7f
samuelowino <samuelowino43@yahoo.com>
2022-11-10 22:08:48 +0300

    launch ios app project

---
+++ chronos-ios/chronos-ios/chronos_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  chronos_iosApp.swift
+//  chronos-ios
+//
+//  Created by Samuel Owino on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct chronos_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
